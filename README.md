35b8a8b61ce461af30de1f26238b4d050b6e71c1
@Sanjeeb <120170788+theHalfCoder1dot0@users.noreply.github.com>
2022-12-18 02:33:24 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# string-random-color
+It's a npm package for creating a color based on input string.
---
+++ index.js
@@ -0,0 +1,10 @@
+randomColor = (string, saturation = 50, brightness = 60  ) => {
+    var hash = 0;
+    for (var i = 0; i < string.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    var h = hash % 360;
+    return 'hsl('+h+', '+saturation+'%, '+brightness+'%)';
+  }
+
+export default { randomColor};
